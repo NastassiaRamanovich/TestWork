64f1e8faf3857bc05bca4559bbf8c5113593c147
NastassiaRamanovich <nastassia_ramanovich@mail.ru>
2024-01-08 19:39:31 +0300

    Метод, который определяет длину элемена массива и при выполнении условия добавляет его во второй массив

--- Program.cs
+++ Program.cs
@@ -6,3 +6,17 @@
 
 string[] array1 = new string[8] {"Cat", "Pulya", "Rex", "Belyash", "Fedya", "Pet", "111", "Sun"};
 string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if (array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
